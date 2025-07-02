49dcee37988ab350b01a90749d48a7796d1a8356
Fykamie <eszesztiti@gmail.com>
2025-07-02 21:27:58 +0200

    Add routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,19 @@
 import { Component, HostListener } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { PitBoardComponent } from "./components/pit-board/pit-board.component";
+import { Router, RouterModule, RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { GameService } from './services/game.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CommonModule, PitBoardComponent],
+  imports: [RouterOutlet, CommonModule, RouterModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.less'
 })
 export class AppComponent {
-  constructor(public gameService: GameService) {}
+ 
+  constructor(public gameService: GameService, public router: Router) {
+  }
 
   @HostListener('document:contextmenu', ['$event'])
     onRightClick(event: MouseEvent) {
